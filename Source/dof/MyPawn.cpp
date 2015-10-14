f5348e0b9ccab88039f66851ab22bdd364249b8e
thyshimrod <shimrod@free.fr>
2015-10-14 10:12:39 +0200

    first commit

---
+++ Source/dof/MyPawn.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "dof.h"
+#include "MyPawn.h"
+
+
+// Sets default values
+AMyPawn::AMyPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	this->vitesse = 0.0f;
+}
+
+// Called when the game starts or when spawned
+void AMyPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyPawn::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+// Called to bind functionality to input
+void AMyPawn::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	Super::SetupPlayerInputComponent(InputComponent);
+
+}
+
+void AMyPawn::addAcceleration(float pAcceleration){
+	if (((this->vitesse + pAcceleration) < 30) && ((this->vitesse + pAcceleration) >= 0)){
+		this->vitesse += pAcceleration;
+	}
+	else{
+		if ((this->vitesse + pAcceleration) <= 0){
+			this->vitesse = 0;
+		}
+		else{
+			this->vitesse = 30;
+		}
+	}
+	this->vitesse += pAcceleration;
+}
+
+float AMyPawn::getVitesse()
+{
+	return this->vitesse;
+}
---
+++ Source/dof/MyPawn.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Pawn.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MyPawn.generated.h"
+
+UCLASS()
+class DOF_API AMyPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AMyPawn();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "ship")
+		float vitesse;
+
+	UFUNCTION(BlueprintCallable, Category = "ship")
+		void addAcceleration(float pAcceleration);
+
+	UFUNCTION(BlueprintCallable, Category = "ship")
+		float getVitesse();
+
+
+	
+	
+};
